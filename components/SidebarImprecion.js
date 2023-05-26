fcb906c22f3de21dba5f49de6229d6d63f3dc0b5
unknown <jerez4959@gmail.com>
2023-05-26 14:22:14 -0400

    first commitwwwasasSSSSAaddaddllljjasASASaswwwlaAtarrseaea

--- components/SidebarImprecion.js
+++ components/SidebarImprecion.js
@@ -1,6 +1,6 @@
 export const Sidebarimprecion = () => {
     return (
-      <div class="px-3 py-4 overflow-y-auto rounded bg-white">
+      <div class="px-3 py-1 overflow-y-auto rounded bg-white">
         <ul class="space-y-2">
         </ul>
 
