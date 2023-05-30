3dc9280c7bc1153ad2da808611c815b75ebb33ac
unknown <jerez4959@gmail.com>
2023-05-30 10:57:26 -0400

    first commitwwwasasSSSSAaddaddllljjasASASaswwwlaAtarrseaeassas

--- pages/listado-total-admin.js
+++ pages/listado-total-admin.js
@@ -469,7 +469,7 @@ export default function InformeAgr() {
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Produccion</td>
                                                 <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoemp)} m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalVolumensemp / 162 * 189)} m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalVolumensemp / 171 * 189)} m³</td>   
                                             </tr>
                                         </tbody>
                                         </table>
@@ -499,7 +499,7 @@ export default function InformeAgr() {
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Produccion</td>
                                                 <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco)} m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 672 * 744)} m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 696 * 744)} m³</td>   
                                             </tr>
                                         </tbody>
                                         </table>
