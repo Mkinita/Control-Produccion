ef0f41410e6d4ea3b65426dd37dcddbceef86a4e
unknown <jerez4959@gmail.com>
2023-06-14 17:33:01 -0400

    agr20aasasasasas

--- pages/listado-total-admin.js
+++ pages/listado-total-admin.js
@@ -389,7 +389,7 @@ export default function InformeAgr() {
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Ingreso</td>
                                                 <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresodzo)}</td> 
-                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresodzo / 4860)} Trozos / Minuto</td>  
+                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresodzo / 5400)} Trozos / Minuto</td>  
                                             </tr>
                                         </tbody>
                                         </table>
@@ -505,7 +505,7 @@ export default function InformeAgr() {
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Produccion</td>
                                                 <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoemp)} m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalVolumensemp / 81 * 180)} m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalVolumensemp / 90 * 180)} m³</td>   
                                             </tr>
                                         </tbody>
                                         </table>
@@ -537,7 +537,7 @@ export default function InformeAgr() {
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Produccion</td>
                                                 <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco)} m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 312 * 720)} m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 336 * 720)} m³</td>   
                                             </tr>
                                         </tbody>
                                         </table>
