55c302191cc9c2ca1476f1bd5d789bd578d5e456
unknown <jerez4959@gmail.com>
2023-07-18 09:57:49 -0400

    provandosdaslllsadasaasaddds

--- components/StockTerminado.js
+++ components/StockTerminado.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 
 const StockTerminado = ({orden}) => {
-  const {id, nombre, total, pedido, fecha, folio, descripcion, cantidad, calidad} = orden
+  const {id, nombre, total, pedido, fecha, folio, descripcion, cantidad, calidad,cliente} = orden
   
 
   
@@ -17,11 +17,12 @@ const StockTerminado = ({orden}) => {
           <tbody className="text-center">
             {pedido.map(oc => (
               <tr className="bg-white border-b hover:bg-lime-300 text-sm" key={oc.id}>
-                <td className="px-6 py-4 w-1/5 text-center border border-lime-400">{oc.espesor}</td>
-                <td className="px-6 py-4 w-1/5 text-center border border-lime-400">{oc.ancho}</td>
-                <td className="px-6 py-4 w-1/5 text-center border border-lime-400">{oc.largo}</td>
-                <td className="px-6 py-4 w-1/5 text-center border border-lime-400">{formatoNumero(oc.espesor * oc.ancho * oc.largo * oc.piezas *(oc.cantidad - cantidad) / 1000000 )}</td>
-                <td className="px-6 py-4 w-1/5 text-center border border-lime-400">{nombre}</td>
+                <td className="px-6 py-4 w-1/6 text-center border border-lime-400">{oc.espesor}</td>
+                <td className="px-6 py-4 w-1/6 text-center border border-lime-400">{oc.ancho}</td>
+                <td className="px-6 py-4 w-1/6 text-center border border-lime-400">{oc.largo}</td>
+                <td className="px-6 py-4 w-1/6 text-center border border-lime-400">{formatoNumero(oc.espesor * oc.ancho * oc.largo * oc.piezas *(oc.cantidad - cantidad) / 1000000 )}</td>
+                <td className="px-6 py-4 w-1/6 text-center border border-lime-400">{nombre}</td>
+                <td className="px-6 py-4 w-1/6 text-center border border-lime-400">{cliente}</td>
               </tr>
             ))}
           </tbody>
--- pages/listado-total-admin.js
+++ pages/listado-total-admin.js
@@ -552,7 +552,7 @@ export default function InformeAgr() {
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Produccion</td>
                                                 <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco)} m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 360 * 744)} m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 384 * 744)} m³</td>   
                                             </tr>
                                         </tbody>
                                         </table>
