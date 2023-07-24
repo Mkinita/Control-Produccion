eab1b67d1f1c57b20f52bce4270f80966b9e5b02
unknown <jerez4959@gmail.com>
2023-07-24 09:37:30 -0400

    22-07-2023

--- pages/listado-total.js
+++ pages/listado-total.js
@@ -567,7 +567,7 @@ export default function InformeAgr() {
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Produccion</td>
                                                 <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco)} m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 456 * 744)} m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">{formatoNumero(totalIngresoseco  / 504 * 744)} m³</td>   
                                             </tr>
                                         </tbody>
                                         </table>
