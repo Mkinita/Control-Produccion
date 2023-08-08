f84c7077341c6d2d2351f96a5aaf48d7a6a1789b
unknown <jerez4959@gmail.com>
2023-08-08 09:03:05 -0400

    277-0777-202333aaas

--- pages/listado-empalillado.js
+++ pages/listado-empalillado.js
@@ -145,9 +145,9 @@ export default function AdminProducciones() {
                             <td className="px-2 py-4 w-1/5 text-center">Total</td>
                             
                             <td className="px-2 py-4 w-1/5 text-center">{formatoNumero(totalVolumens)}</td>
-                            <td className="px-2 py-4 w-1/5 text-center">{formatoNumeroEmp(totalVolumens / 36)}</td>
+                            <td className="px-2 py-4 w-1/5 text-center">{formatoNumeroEmp(totalVolumens / 45)}</td>
                             <td className="px-2 py-4 w-1/5 text-center">{formatoNumeroDsp(totalIngreso)}</td>
-                            <td className="px-2 py-4 w-1/5 text-center">{formatoNumeroDsp(totalIngreso / 36)}</td>
+                            <td className="px-2 py-4 w-1/5 text-center">{formatoNumeroDsp(totalIngreso / 45)}</td>
                         </tr>
                     
                 </tbody>
