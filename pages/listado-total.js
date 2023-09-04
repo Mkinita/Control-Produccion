7728194e7f576cfcb39a42c928af663c677ddfaa
unknown <jerez4959@gmail.com>
2023-09-04 10:40:11 -0300

    nuevo12324568910

--- pages/listado-total.js
+++ pages/listado-total.js
@@ -527,14 +527,14 @@ export default function InformeAgr() {
 
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Seco</td>
-                                                <td className="border border-lime-200 px-1 py-2">0 m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">0 m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">11.3 m³</td>
+                                                <td className="border border-lime-200 px-1 py-2">214.7 m³</td>   
                                             </tr>
 
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Total</td>
-                                                <td className="border border-lime-200 px-1 py-2 text-center">{formatoNumero(totalIngresoemp + 0)} m³</td>
-                                                <td className="border border-lime-200 px-1 py-2 text-center">{formatoNumero(totalVolumensemp /TOTAL * TOTAL_ + 822.1)} m³</td>
+                                                <td className="border border-lime-200 px-1 py-2 text-center">{formatoNumero(totalIngresoemp + 214.7)} m³</td>
+                                                <td className="border border-lime-200 px-1 py-2 text-center">{formatoNumero(totalVolumensemp /TOTAL * TOTAL_ + 11.3)} m³</td>
                                                
                                             </tr>
 
