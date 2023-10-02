8d53a016c91f6a57ee89d78f88f1f1b55af6d086
unknown <jerez4959@gmail.com>
2023-10-02 16:46:41 -0300

    101200

--- pages/api/listado-producciones.js
+++ pages/api/listado-producciones.js
@@ -1,23 +1,39 @@
+// import { PrismaClient } from "@prisma/client";
+
+// export default async function handler(req, res) {
+//   const prisma = new PrismaClient();
+//   const fecha = new Date();
+//   const year = fecha.getFullYear();
+//   const month = fecha.getMonth();
+//   const day = fecha.getDate();
+//   // Obtener Ordenes
+//   const ordenes = await prisma.orden.findMany({
+//     where: {
+//       estado: true,
+//       AND: [
+//         {
+//           fecha: {
+//             gt: new Date(year, month, day),
+//           },
+//         },
+//       ],
+//     },
+//   });
+//   res.status(200).json(ordenes);
+// }
+
+
 import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
-  const fecha = new Date();
-  const year = fecha.getFullYear();
-  const month = fecha.getMonth();
-  const day = fecha.getDate();
-  // Obtener Ordenes
+  //Obtener Ordenes
   const ordenes = await prisma.orden.findMany({
     where: {
-      estado: true,
-      AND: [
-        {
-          fecha: {
-            gt: new Date(year, month, day),
-          },
-        },
-      ],
-    },
-  });
+        estado: true,
+      }
+
+  })
+
   res.status(200).json(ordenes);
 }
