159b1da732310efdc49f1368e84122833eb36bc6
unknown <jerez4959@gmail.com>
2023-10-02 16:53:39 -0300

    1012001

--- pages/api/listado-producciones.js
+++ pages/api/listado-producciones.js
@@ -23,17 +23,31 @@
 // }
 
 
+
 import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
-  //Obtener Ordenes
+  const fecha = new Date();
+  const year = fecha.getFullYear();
+  const month = fecha.getMonth();
+  const day = fecha.getDate();
+
+  // Calcular la fecha límite (2:00 AM del día siguiente)
+  const limite = new Date(year, month, day + 1, 2, 0, 0);
+
+  // Obtener Ordenes hasta las 2:00 AM del día actual
   const ordenes = await prisma.orden.findMany({
     where: {
-        estado: true,
-      }
-
-  })
+      estado: true,
+      fecha: {
+        gte: new Date(year, month, day, 0, 0, 0), // Desde las 00:00:00 del día actual
+        lt: limite, // Hasta las 2:00 AM del día siguiente
+      },
+    },
+  });
 
   res.status(200).json(ordenes);
 }
+
+
