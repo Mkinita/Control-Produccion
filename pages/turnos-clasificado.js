e5419b55e8266cb35410dacf8dd7b07340a977ae
unknown <jerez4959@gmail.com>
2023-10-13 02:30:26 -0300

    101200123456478900123

--- pages/turnos-clasificado.js
+++ pages/turnos-clasificado.js
@@ -49,9 +49,15 @@ const turnos = () => {
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400">83.4 m³</td>
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400">81.5 m³</td>    
                     </tr>
+
+                    <tr className='hover:bg-amber-300 text-sm'>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">12-10-2023</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">72.4 m³</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">57.7 m³</td>    
+                    </tr>
                     <tr className='text-sm'>
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold">Total</td> 
-                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold" colSpan="2">1237.2 m³</td>    
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold" colSpan="2">1366.5 m³</td>    
                     </tr>
                 </tbody>
             </table>
