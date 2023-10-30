2297da87977a1a651be61ff01bec9de4e57a7209
unknown <jerez4959@gmail.com>
2023-10-30 12:54:12 -0300

    0001234456789009

--- pages/turnos-clasificado.js
+++ pages/turnos-clasificado.js
@@ -92,6 +92,21 @@ const turnos = () => {
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400">127.4 m³</td>
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400">122.1 m³</td>    
                     </tr>
+                    <tr className='hover:bg-amber-300 text-sm'>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">24-10-2023</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">85.1 m³</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">82.8 m³</td>    
+                    </tr>
+                    <tr className='hover:bg-amber-300 text-sm'>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">25-10-2023</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">85.4 m³</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">85.7 m³</td>    
+                    </tr>
+                    <tr className='hover:bg-amber-300 text-sm'>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">26-10-2023</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">96.7 m³</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">72.1 m³</td>    
+                    </tr>
                     <tr className='text-sm'>
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold">Total</td> 
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold" colSpan="2">2752.2 m³</td>    
