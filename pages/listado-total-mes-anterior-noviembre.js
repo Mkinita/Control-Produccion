614998f9695fc3576e12a59e5e8de9994a4578b7
unknown <jerez4959@gmail.com>
2023-12-04 16:44:03 -0300

    00012344567890098888800001222322222220

--- pages/listado-total-mes-anterior-noviembre.js
+++ pages/listado-total-mes-anterior-noviembre.js
@@ -145,8 +145,8 @@ export default function InformeAgr() {
                                             
                                             <tr>
                                                 <td className="border border-lime-200 px-1 py-2">Produccion</td>
-                                                <td className="border border-lime-200 px-1 py-2">0 m³</td>
-                                                <td className="border border-lime-200 px-1 py-2">0 m³</td>   
+                                                <td className="border border-lime-200 px-1 py-2">765 m³</td>
+                                                <td className="border border-lime-200 px-1 py-2">765 m³</td>   
                                             </tr>
                                         </tbody>
                                         </table>
