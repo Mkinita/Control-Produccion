b55eaf86afec8de5871a40dc8cb320d3c0ab85ed
unknown <jerez4959@gmail.com>
2024-02-12 10:31:05 -0300

    octavo0

--- components/SidebarInformeAgr.js
+++ components/SidebarInformeAgr.js
@@ -134,6 +134,13 @@ export const SidebarAdmin = () => {
                   <span class="ml-3">Diciembre</span>
               </Link>
            </li>
+
+           <li>
+              <Link href="/listado-total-mes-anterior-enero-2024" class="flex items-center p-2 text-xs font-black text-black rounded-lg bg-lime-400 dark:text-black hover:bg-gray-100 dark:hover:bg-lime-500 uppercase">
+                  📊
+                  <span class="ml-3">Enero-2024</span>
+              </Link>
+           </li>
            <li>
               <Link href="/listado-total" class="flex items-center p-2 text-xs font-black text-black rounded-lg bg-lime-400 dark:text-black hover:bg-gray-100 dark:hover:bg-lime-500 uppercase">
                   📊
