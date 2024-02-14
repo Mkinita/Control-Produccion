cc5668b73bf690cc29b5bc53dc2ff98927a4e454
unknown <jerez4959@gmail.com>
2024-02-14 09:30:48 -0300

    noveno00

--- pages/turnos-emp.js
+++ pages/turnos-emp.js
@@ -25,14 +25,20 @@ const turnos = () => {
                     </tr>
 
                     <tr className='hover:bg-amber-300 text-sm'>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">13-02-2024</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">163.0 m³</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">189.0 m³</td>    
+                    </tr>
+
+                    <tr className='hover:bg-amber-300 text-sm'>
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400">Total Turnos</td>
-                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">1031.1 m³</td>
-                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">1075.0 m³</td>    
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">1194.1 m³</td>
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400">1264.0 m³</td>    
                     </tr>
                    
                     <tr className='text-sm'>
                         <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold">Total</td> 
-                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold" colSpan="2">1147 m³</td>    
+                        <td className="px-1 py-2 w-1/3 text-center border border-amber-400 font-semibold" colSpan="2">1499.0 m³</td>    
                     </tr>
                 </tbody>
             </table>
