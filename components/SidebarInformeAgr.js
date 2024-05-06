9f32c05549efab0caac57e344344f01edfccb291
unknown <jerez4959@gmail.com>
2024-05-06 11:10:30 -0400

    onetree

--- components/SidebarInformeAgr.js
+++ components/SidebarInformeAgr.js
@@ -184,6 +184,13 @@ export const SidebarAdmin = () => {
            </li>
 
            <li>
+              <Link href="/listado-total-mes-anterior-abril-2024" class="flex items-center p-2 text-xs font-black text-black rounded-lg bg-lime-400 dark:text-black hover:bg-gray-100 dark:hover:bg-lime-500 uppercase">
+                  📊
+                  <span class="ml-3">Abril-2024</span>
+              </Link>
+           </li>
+
+           <li>
               <Link href="/listado-total" class="flex items-center p-2 text-xs font-black text-black rounded-lg bg-lime-400 dark:text-black hover:bg-gray-100 dark:hover:bg-lime-500 uppercase">
                   📊
                   <span class="ml-3">{currentMonth}-2024</span>
